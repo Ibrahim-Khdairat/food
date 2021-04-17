ba2300e40f469ccf61c95f1e3c598c361439ce80
IbrahimKhdairat <>
2021-04-17 14:15:33 +0000

    new updates

--- java1.js
+++ java1.js
@@ -1,11 +1,6 @@
 var x =  Number(prompt ('Enter a number to know if it is positive or negative '));
 
-<<<<<<< HEAD
-=======
-while (x>=0 || x<0 )
-{
 
->>>>>>> 0dd53864f69023834301edc3006ea93d9701bb82
 if (x>=0)
 {
   alert ( x+ " Is a Positive Number " );
@@ -15,8 +10,7 @@ else
 {
  alert (x+ " Is a Negative Number " );
 }
-<<<<<<< HEAD
-=======
+
 x =  Number(prompt ('Enter a number to know if it is positive or negative '));
 }
 >>>>>>> 0dd53864f69023834301edc3006ea93d9701bb82
