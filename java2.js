3c260902125402e0b6f7d0e56ddceb5e3ad00eeb
IbrahimKhdairat <>
2021-04-17 17:59:36 +0000

    file name update

--- java2.js
+++ java2.js
@@ -6,16 +6,20 @@ var z = Number (prompt('Enter the third number '));
 if (x>y && x>z)
 {
   console.log(x+  ' Is the max number');
+    document.write(x+ " Is the max number ");
+
 }
 
 else if ( y>x && y>z)
 {
   console.log(y+  ' Is the max number');
+    document.write(y+ " Is the max number ");
 }
 
 else if (z>x && z>y)
 {
   console.log(z+  ' Is the max number');
+    document.write(z+ " Is the max number ");
 }
 
 
--- java3.js
+++ java3.js
@@ -1,13 +1,17 @@
 console.log('Sample Output : ');
-
+  document.write('Sample Output : ');
 for ( i=0;i<=20;i++)
 {
  if (i % 2 == 0)
   {
     console.log(i+' Is Even');
+     document.write(i+' Is Even');
+      document.write('<br>');
   } 
  else 
   {
   console.log(i+' Is Odd');
+       document.write(i+' Is Odd');
+ document.write('<br>');
   }
 }
