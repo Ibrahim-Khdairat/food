287e6df85a2a02861706991721b39b3789aa8ce3
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-05-02 01:20:53 -0700

    Example of Stars Level

---
+++ java6.js
@@ -0,0 +1,13 @@
+let input = Number(prompt("Please Enter The Level For Stars !!!"));
+
+for (let i = 1; i <=1; i++) 
+{
+    for (let j = 1; j <= input; j++) 
+    {
+        document.write('*'.repeat(j));
+        document.write('<br />');
+
+    }
+
+
+}
